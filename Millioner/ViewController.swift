8e0bd825e1e5accf772f218cd1e6e614fccb6add
GBrovina <bunny.brovina@yandex.ru>
2020-06-02 19:07:51 +0300

    Initial Commit

---
+++ Millioner/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Millioner
+//
+//  Created by Галина  Бровина  on 02.06.2020.
+//  Copyright © 2020 Галина  Бровина . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
